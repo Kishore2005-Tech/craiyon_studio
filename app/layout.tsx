10517e972dd23ffb1c68195ef0bc1045926dd0dd
Kishore-Dev <kishoreprakash45@gmail.com>
2026-08-16 23:12:19 +0530

    Remove media query for light icon
    Removed media query for light icon.

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
     icon: [
       {
         url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
+        
       },
       {
         url: '/icon-dark-32x32.png',
